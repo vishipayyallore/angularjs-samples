80e69c85881c202c451d5ee1f5410896287fc668
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-07-26 20:51:17 +0530

    Added e2e tests too.

--- angularjs-phonecat/e2e-tests/scenarios.js
+++ angularjs-phonecat/e2e-tests/scenarios.js
@@ -1,11 +1,29 @@
-"use strict";
 
+"use strict";
 // Angular E2E Testing Guide:
 // https://docs.angularjs.org/guide/e2e-testing
 
-describe("Phone Catelog",function(){
+describe("Phone Catalog Application", function () {
 
-    beforeEach(function(){
-        browser.get("index.html");
+    describe("phoneList", function () {
+
+        beforeEach(function () {
+            browser.get("index.html");
+        });
+
+        it("Should filter the phone list as a user types into the search box", function(){
+            var phoneList = element.all(by.repeater("phone in vm.phones"));
+            var queryOn = element(by.model("vm.queryOn"));
+
+            expect(phoneList.count()).toBe(8);
+
+            queryOn.sendKeys('nexus');
+            expect(phoneList.count()).toBe(6);
+            
+            queryOn.sendKeys('moto');
+            expect(phoneList.count()).toBe(4);
+        });
+
     });
+
 });
