059c5fc95c4010f78e24772532e569011312eaf0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-07-27 17:52:49 +0530

    Added the Dropdown functionality.

--- angularjs-phonecat/app/PhoneList/phone-list.controller.js
+++ angularjs-phonecat/app/PhoneList/phone-list.controller.js
@@ -1,32 +1,41 @@
 
-(function(){
+(function () {
 
     "use strict";
 
     angular.module("phoneList")
         .controller('PhoneListController', ["$scope", PhoneListController]);
 
-        function PhoneListController($scope){
-            var vm = this;
-            
-            vm.queryOn = '';
-            vm.phones = [
-                {
-                name: 'Nexus S',
-                snippet: 'Fast just got faster with Nexus S.'
-                }, {
-                name: 'Nexus S2',
-                snippet: 'Fast just got faster with Nexus S2.'
-                },{
-                name: 'Motorola XOOM™ with Wi-Fi',
-                snippet: 'The Next, Next Generation tablet.'
-                }, {
-                name: 'MOTOROLA XOOM™',
-                snippet: 'The Next, Next Generation tablet.'
-                }
-            ];
+    function PhoneListController($scope) {
+        var vm = this;
 
-            
-        }
+        vm.queryOn = '';
+        vm.orderBy = 'age';
+        vm.orderByOptions = [
+            { id: "name", desc: "Name" },
+            { id: "age", desc: "Age" }
+        ];
+
+        vm.phones = [
+            {
+                name: 'Nexus S',
+                snippet: 'Fast just got faster with Nexus S.',
+                age: 1
+            }, {
+                name: 'Nexus S2 with Wi-Fi',
+                snippet: 'Fast and faster with Nexus S2.',
+                age: 2
+            }, {
+                name: 'Motorola XOOM™ with Wi-Fi',
+                snippet: 'The Next, Next Generation tablet.',
+                age: 3
+
+            }, {
+                name: 'MOTOROLA XOOM™',
+                snippet: 'The Next, Next Generation tablet.',
+                age: 4
+            }
+        ];
+    }
 
 })();
