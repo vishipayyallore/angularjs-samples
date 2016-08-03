ab0313c3c66732ab60b617914fcc08012fc54240
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-08-03 20:44:52 +0530

    Adding new functionlity

---
+++ angularjs-phonecat/app/app.config.js
@@ -0,0 +1,22 @@
+
+(function () {
+    
+    "use strict";
+
+    angular.module("phonecatApp")
+        .config(["$stateProvider", "$locationProvider", "$urlRouterProvider", initRouter]);
+
+
+    function initRouter($stateProvider, $locationProvider, $urlRouterProvider){
+        $locationProvider.html5Mode(true);
+        $urlRouterProvider.otherwise("/");
+
+        $stateProvider
+            .state( "home", {
+                url: "/",
+                templateUrl: "app/home/index.html",
+                controllerAs: "vm"
+            });
+	}
+
+})();
